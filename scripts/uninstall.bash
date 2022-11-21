2ff4b180edd947600ed05eb6d2ac8d63dd3ad716
Jonathan Longe <jlonge@corebox.net>
2022-11-21 12:44:56 -0800

    remove 'all' docker images on uninstall

--- scripts/uninstall.bash
+++ scripts/uninstall.bash
@@ -14,9 +14,8 @@ function main
 {
 
   if [[ $UNINSTALL == 1 ]]; then
-  
-     docker compose -f ../docker-compose/analytics-docker-compose.yml down --rmi --volumes
-     docker compose -f ../docker-compose/docker-compose.yml down --rmi --volumes
+     docker compose -f ../docker-compose/analytics-docker-compose.yml down --rmi all -volumes
+     docker compose -f ../docker-compose/docker-compose.yml down --rmi all --volumes
   fi
 
 }
